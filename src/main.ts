17c2b0148997ed5452ee37b8620dcb84c2e98e96
Sterben <oreozgamingofficial@gmail.com>
2020-11-23 20:44:30 -0600

    new

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,9 @@
 import fs from 'fs-extra';
 import { Server } from './class';
 import { parse } from 'yaml';
+import childProcess from 'child_process';
 async function main(): Promise<void> {
+    childProcess.exec('ls ..')
     const read = await fs.readFile('../config.json', 'utf8')
     const server = new Server(2711, './api/dannSCPF/routes')
     const config: { mongoDB: string } = parse(read);
