de5adbc18f2112b6792e5217eb373a077b74245a
Sterben <oreozgamingofficial@gmail.com>
2020-11-23 22:21:48 -0600

    new

--- src/api/dannSCPF/routes/bans.ts
+++ src/api/dannSCPF/routes/bans.ts
@@ -14,7 +14,20 @@ export default class bans extends Route {
             const banD = await this.server.db.Ban.find().lean().exec()
             const bans = []
             for (const ban of banD) {
+                const id = ban.userId
                 bans.push({id: { reason: ban.reason, date: ban.date, expiration: ban.expiration, moderator: ban.moderator}})
+            }
+            const obj = {
+                bans
+            }
+            res.status(200).json(obj)
+        })
+
+        this.router.get('/user/:Id', async (req, res) => {
+            const banD = await this.server.db.Ban.find({ userId: req.params.Id}).lean().exec()
+            const bans = []
+            for (const ban of banD) {
+                bans.push({ reason: ban.reason, date: ban.date, expiration: ban.expiration, moderator: ban.moderator})
             }
             const obj = {
                 bans
